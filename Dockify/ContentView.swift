5209c49ef4c96a77d43be2b588da48a63c334e2c
Mohd Abdul Subhan <masubhan890@gmail.com>
2024-11-26 17:30:05 -0500

    signup and login design completed
    TODO: Get started introduction screens

--- Dockify/ContentView.swift
+++ Dockify/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+                    Color.blue
+                        .ignoresSafeArea()
+                    Text("ContentView")
+                        .foregroundColor(.white)
+                        .font(.system(size: 30))
+                        .bold()
+                        .padding()
+                }
     }
 }
 
---
+++ Dockify/Styles/CustomStyleModifier.swift
@@ -0,0 +1,71 @@
+//
+//  CustomStyleModifier.swift
+//  Dockify
+//
+//  Created by Mohd Abdul Subhan on 11/26/24.
+//
+
+import SwiftUI
+
+struct CustomStyleModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).applyInputStyle()
+    }
+}
+
+#Preview {
+    CustomStyleModifier()
+}
+
+
+// Modifier for Text Style
+struct TextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.black)
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(8)
+    }
+}
+
+// Modifier for Button Style
+struct ButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(12)
+            .shadow(radius: 5)
+    }
+}
+
+// Modifier for Button Style
+struct InputStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title3)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(50.0)
+            .shadow(color: Color.black.opacity(0.08), radius: 60, x: 0.0, y: 16)
+    }
+}
+    
+extension View {
+    func applyTextStyle() -> some View {
+        self.modifier(TextStyle())
+    }
+    
+    func applyInputStyle() -> some View {
+        self.modifier(InputStyle())
+    }
+    
+    func applyButtonStyle() -> some View {
+        self.modifier(ButtonStyle())
+    }
+}
